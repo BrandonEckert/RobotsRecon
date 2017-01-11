ed98271b40f147bfc98c10d91d879d89697d221f
BrandonEckert <brandonjeckert@gmail.com>
2017-01-10 21:35:32 -0500

    Add files via upload

--- robotsrecon.py
+++ robotsrecon.py
@@ -26,10 +26,10 @@
 #
 # Usage: python ./robotsrecon.py http://<ip address or domain name>
 
-import requests
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import os
 import sys
+import requests
 
 # set text color class
 
@@ -46,44 +46,66 @@ class bcolors:
 
 # generate banner, clear screen, print banner
 def gen_banner():
-    print(r"""
+    print((r"""
          (\____/)
           (_""" + bcolors.RED + r"""oo""" + bcolors.ENDC + r""" _)
             (O)
           __||__    \)
-       []/______\[] /
+       []/""" + bcolors.BLUE + r"""______""" + bcolors.ENDC + r"""\[] /
        / \______/ \/
       /    /__\
      (\   /____\
 
      """ + bcolors.BOLD + """RobotsRecon""" + bcolors.ENDC + """
-        """)
+        """))
 
 os.system('clear')
 gen_banner()
-print "RobotsRecon - Reads website robots.txt from website,"
-print "and checks disallow if directories exist"
-print "Written by: Brandon Eckert"
-print ""
-print "Website: http://www.brandoneckert.com"
-print ""
-print "Usage: python ./robotsrecon.py http://<ip address or domain name>"
-print "Parsing robots.txt..."
-print ""
+print("RobotsRecon - Reads robots.txt from website,")
+print("and checks disallow if directories exist.")
+print("Written by: Brandon Eckert")
+print("")
+print("Website: http://www.brandoneckert.com")
+print("")
+print("Usage: python3 ./robotsrecon.py http://<ip address or domain name>")
+print("")
 
-# get url from parameter and get robots.txt
+# get url from parameter
 try:
     urlstring = (sys.argv[1])
-    lines = urllib2.urlopen(urlstring + '/robots.txt')
-    # read robots.txt check each directory for non 404
-    for line in lines:
-        if line[0] != '#' and line[0].strip() != '':
-            lineSplit = line.split(': ')
-            if lineSplit[0].lower() == 'allow' or lineSplit[0].lower() == 'disallow':
-                targetUrl = urlstring + '%s' % lineSplit[1].strip()
-                r = requests.get(targetUrl)
-                if r.status_code != 404:
-                    print bcolors.GREEN + "FOUND: " + targetUrl + bcolors.ENDC
-
 except Exception:
-    print bcolors.RED + "Exception: Did you forget parameter? Or is website live?" + bcolors.ENDC
+    print ((bcolors.RED + "Exception: Did you forget IP/URL?" + bcolors.ENDC))
+    exit()
+# Attempt to find robots.txt
+try:
+    print ((bcolors.BLUE + "Attempting to download robots.txt..." + bcolors.ENDC))
+    urllib.request.urlretrieve(urlstring + '/robots.txt', 'robots.txt')
+    print ((bcolors.GREEN + "Download complete..." + bcolors.ENDC))
+except Exception:
+    print ((bcolors.RED + "Exception: Unable to find robots.txt. Does it exist? or is the site up?" + bcolors.ENDC))
+    exit()
+# Attempt to read robots.txt line by line
+print ((bcolors.BLUE + "Checking status of disallowed directories..." + bcolors.ENDC))
+robotsfile = tuple(open('robots.txt', 'r'))
+try:
+    for line in robotsfile:
+            if line[0] != '#' and line[0].strip() != '':
+                    lineSplit = line.split(': ')
+                    if lineSplit[0].lower() == 'disallow':
+                            targetUrl = urlstring +'%s' % lineSplit[1].strip()
+                            response = requests.get(targetUrl)
+                            if response.status_code != 404:
+                                    print ((bcolors.GREEN + "FOUND:" + targetUrl + bcolors.ENDC))
+                            else:
+                                    print ((bcolors.RED + "NOT FOUND:" + targetUrl + bcolors.ENDC))
+except Exception:
+    print ((bcolors.RED + "EXCEPTION: Something has occured while parsing robots.txt..." + bcolors.ENDC))
+    exit()
+
+# Cleanup
+try:
+    print ((bcolors.BLUE + "Cleaning up..." + bcolors.ENDC))
+    os.remove('robots.txt')
+except Exception:
+    pass
+
